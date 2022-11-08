1f7cbce34981fb7ae5c7fa6cadae8097be2db900
Yaroslava <karpenko16yaroslava@gmail.com>
2022-11-08 21:42:53 +0200

    redone

--- lab3/ex1.py
+++ lab3/ex1.py
@@ -1,95 +1,168 @@
 import json
+import uuid
+from datetime import datetime, date
+
+
+class Event:
+    def __init__(self, name, year, month, day, hour, minute):
+        self.name = name
+        now = datetime.now()
+        str_d = f'{year}-{month}-{day} {hour}:{minute}'
+        d = datetime.strptime(str_d, '%Y-%m-%d %H:%M')
+        if d > now:
+            self.year = year
+            self.month = month
+            self.day = day
+            self.hour = hour
+            self.minute = minute
+        else:
+            raise ValueError('Event has already passed')
+
+    @property
+    def year(self):
+        return self.__year
+
+    @year.setter
+    def year(self, value):
+        if not isinstance(value, int):
+            raise TypeError
+        self.__year = value
+
+    @property
+    def month(self):
+        return self.__month
+
+    @month.setter
+    def month(self, value):
+        if not isinstance(value, int):
+            raise TypeError
+        if not 0 < value <= 12:
+            raise ValueError
+        self.__month = value
+
+    @property
+    def day(self):
+        return self.__day
+
+    @day.setter
+    def day(self, value):
+        if not isinstance(value, int):
+            raise TypeError
+        if not 0 < value <= 31:
+            raise ValueError
+        self.__day = value
+
+    @property
+    def hour(self):
+        return self.__hour
+
+    @hour.setter
+    def hour(self, value):
+        if not isinstance(value, int):
+            raise TypeError
+        if not 0 <= value <= 24:
+            raise ValueError
+        self.__hour = value
+
+    @property
+    def minute(self):
+        return self.__minute
+
+    @minute.setter
+    def minute(self, value):
+        if not isinstance(value, int):
+            raise TypeError
+        if not 0 <= value <= 60:
+            raise ValueError
+        self.__minute = value
+
+    def __str__(self):
+        return f'Event: "{self.name}"\nDate: {self.day}.{self.month}\nTime: {self.hour}:{self.minute}\n'
 
 
 class RegularTicket:
-    def __init__(self, name, date, time, number, price):
-        self.name = name
-        self.date = date
-        self.time = time
-        self.number = number
-        self.price = price
 
-    @property
-    def number(self):
-        return self.__number
+    def __init__(self, price, event, discount=1.0, title='Regular'):
+        self.title = title
+        self.number = uuid.uuid4()
+        self.price = round(price * discount)
+        self.event = event
+        self.__json_serialize(event)
 
-    @number.setter
-    def number(self, number):
-        if not isinstance(number, int):
-            raise TypeError
-        if number <= 0:
-            raise ValueError
-        data = open('tickets.json')
-        lis = json.load(data)
-        for item in lis:
-            if number == item:
-                raise TypeError
-        self.__number = number
-        lis.append(number)
+    def __json_serialize(self, event):
+        with open('tickets.json', 'r') as tick:
+            lis = json.load(tick)
+        if 'tickets' not in lis:
+            lis['tickets'] = {}
+        if str(self.number) not in lis['tickets']:
+            lis['tickets'][str(self.number)] = {}
+            lis['tickets'][str(self.number)]['name'] = event.name
+            lis['tickets'][str(self.number)]['date'] = f'{event.year}.{event.month}'
+            lis['tickets'][str(self.number)]['time'] = f'{event.hour}:{event.minute}'
+            lis['tickets'][str(self.number)]['title'] = self.title
+            lis['tickets'][str(self.number)]['price'] = self.price
         with open('tickets.json', 'w') as tick:
-            json.dump(lis, tick)
+            json.dump(lis, tick, indent=4)
 
-    def get_regular_price(self):
-        return f'Regular ticket. Price:{self.price}'
+    def get_price(self):
+        return self.price
 
-    def get_regular_ticket(self):
-        return f'Ticket №{self.number}\n' \
-               f'Event "{self.name}"\n'\
-               f'Date: {self.date}   Time: {self.time}\n' \
-               f'Price: {self.price}'
+    @classmethod
+    def get_ticket(cls, number):
+        with open('tickets.json', 'r') as tick:
+            lis = json.load(tick)
+        if 'tickets' not in lis:
+            raise KeyError('No tickets sold')
+        if str(number) not in lis['tickets']:
+            raise KeyError('No ticket with such number')
+
+        name = lis['tickets'][str(number)]['name']
+        day = lis['tickets'][str(number)]['date']
+        time = lis['tickets'][str(number)]['time']
+        title = lis['tickets'][str(number)]['title']
+        price = lis['tickets'][str(number)]['price']
+
+        return f'Event: "{name}"\nDate: {day}\nTime: {time}\n{title} ticket  Number: {number}  Price:{price}'
+
+    def __str__(self):
+        return str(self.event) + f'{self.title} ticket  Number: {self.number}  Price:{self.price}'
 
 
 class AdvanceTicket(RegularTicket):
-    def __init__(self, name, date, time, number, price):
-        super().__init__(name, date, time, number, price)
-        self.advancePrice = self.price * 0.4
-
-    def get_advance_price(self):
-        return f'Advance ticket. Price:{self.advancePrice}'
-
-    def get_advance_ticket(self):
-        return f'Ticket №{self.number}\n' \
-               f'Event "{self.name}"\n' \
-               f'Date: {self.date}   Time: {self.time}\n' \
-               f'Price: {self.advancePrice}'
+    def __init__(self, price, event, discount):
+        self.title = 'Advance'
+        super().__init__(price, event, discount, self.title)
 
 
 class StudentTicket(RegularTicket):
-    def __init__(self, name, date, time, number, price):
-        super().__init__(name, date, time, number, price)
-        self.studentPrice = self.price * 0.5
-
-    def get_student_price(self):
-        return f'Student ticket. Price:{self.studentPrice}'
-
-    def get_student_ticket(self):
-        return f'Ticket №{self.number}\n' \
-               f'Event "{self.name}"\n' \
-               f'Date: {self.date}   Time: {self.time}\n' \
-               f'Price: {self.studentPrice}'
+    def __init__(self, price, event, discount):
+        self.title = 'Student'
+        super().__init__(price, event, discount, self.title)
 
 
 class LateTicket(RegularTicket):
-    def __init__(self, name, date, time, number, price):
-        super().__init__(name, date, time, number, price)
-        self.latePrice = round((self.price * 1.1), 2)
-
-    def get_late_price(self):
-        return f'Late ticket. Price:{self.latePrice}'
-
-    def get_late_ticket(self):
-        return f'Ticket №{self.number}\n' \
-               f'Event "{self.name}"\n' \
-               f'Date: {self.date}   Time: {self.time}\n' \
-               f'Price: {self.latePrice}'
+    def __init__(self, price, event, discount):
+        self.title = 'Late'
+        super().__init__(price, event, discount, self.title)
 
 
-ticket1 = StudentTicket("Pride and prejudice", "16.04.22", "18:00", 8, 100)
-print(ticket1.get_student_price())
-print(ticket1.get_student_ticket())
+def order(event, early_term, late_term, student_status=False):
+    today_date = date.today()
+    event_dated = date(event.year, event.month, event.day)
+    res = event_dated - today_date
+    if student_status:
+        return StudentTicket(100, event, 0.5)
+    elif res.days >= early_term:
+        return AdvanceTicket(100, event, 0.4)
+    elif res.days <= late_term:
+        return LateTicket(100, event, 1.1)
+    else:
+        return RegularTicket(100, event)
 
 
-
-
-
-
+event1 = Event('Apple Event', 2022, 11, 20, 12, 30)
+ticket1 = order(event1, 60, 10)
+ticket2 = order(event1, 60, 10, True)
+print(ticket2)
+print(ticket2.get_price())
+print(RegularTicket.get_ticket('a4b05234-3273-40d8-8803-b5c307b190c2'))
--- lab3/ex2.py
+++ lab3/ex2.py
@@ -1,107 +1,107 @@
+from datetime import datetime
+
+addition_ingredients = {'pineapple': 14, 'bacon': 25, 'mushrooms': 9, 'sausages': 20,
+                        'chicken': 20, 'corn': 5, 'olives': 10, 'paprika': 10, 'tomato': 10,
+                        'pepper': 10, 'salami': 20, 'extra cheese': 20}
+
+
 class Pizza:
-    def __init__(self, *args):
-        self.basic = ['sauce', 'cheese']
-        self.ingredients = {'pineapple': 14, 'bacon': 25, 'mushrooms': 9, 'sausages': 20, 'chicken': 20, 'corn': 5,
-                            'olives': 10, 'paprika': 10, 'tomato': 10, 'pepper': 10, 'salami': 20, 'extra cheese': 20}
-        self.extra = []
-        self.price = 0
+    def __init__(self, name, ingredients, price):
+        self.name = name
+        self.components = ingredients
+        self.price = price
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, int | float):
+            raise TypeError
+        if value < 0:
+            raise ValueError
+        self.__price = value
+
+    def agg_ingredients(self, *args):
         for item in args:
-            if self.ingredients[item]:
-                self.extra.append(item)
-                self.price += self.ingredients[item]
-
-
-class Monday(Pizza):
-    def __init__(self, *args):
-        super().__init__(*args)
-        self.m_name = '4 meats'
-        self.m_basic = self.basic + ['salami', 'chicken', 'sausages', 'bacon', 'onion'] + self.extra
-        self.m_price = 109 + self.price
+            if addition_ingredients[item]:
+                self.components.append(item)
+                self.price += addition_ingredients[item]
 
     def __str__(self):
-        return f'Monday pizza: {self.m_name}\n' \
-               f'Ingredients: {self.m_basic}\n' \
-               f'Total price: {self.m_price}'
+        return f'{self.name}\nIngredients: {self.components}\nTotal price: {self.price}'
 
 
-class Tuesday(Pizza):
-    def __init__(self, *args):
-        super().__init__(*args)
-        self.t_name = '4 cheeses'
-        self.t_basic = self.basic + ['dutch cheese', 'mozzarella cheese', 'dor bleu cheese', 'parmesan cheese'] + self.extra
-        self.t_basic.remove('cheese')
-        self.t_price = 105 + self.price
-
-    def __str__(self):
-        return f'Tuesday pizza: {self.t_name}\n' \
-               f'Ingredients: {self.t_basic}\n' \
-               f'Total price: {self.t_price}'
+class FourMeats(Pizza):
+    def __init__(self):
+        self.title = 'Monday: four meats'
+        self.ingredients = ['sauce', 'cheese', 'salami', 'chicken', 'sausages', 'bacon', 'onion']
+        self.price = 109
+        super().__init__(self.title, self.ingredients, self.price)
 
 
-class Wednesday(Pizza):
-    def __init__(self, *args):
-        super().__init__(*args)
-        self.w_name = 'white chicken'
-        self.w_basic = self.basic + ['onion', 'tomato', 'chicken', 'bacon'] + self.extra
-        self.w_price = 99 + self.price
-
-    def __str__(self):
-        return f'Wednesday pizza: {self.w_name}\n' \
-               f'Ingredients: {self.w_basic}\n' \
-               f'Total price: {self.w_price}'
+class FourCheeses(Pizza):
+    def __init__(self):
+        self.title = 'Tuesday: four cheeses'
+        self.ingredients = ['sauce', 'dutch cheese', 'mozzarella cheese', 'dor bleu cheese', 'parmesan cheese']
+        self.price = 105
+        super().__init__(self.title, self.ingredients, self.price)
 
 
-class Thursday(Pizza):
-    def __init__(self, *args):
-        super().__init__(*args)
-        self.th_name = 'hawaiian'
-        self.th_basic = self.basic + ['onion', 'pineapple', 'chicken'] + self.extra
-        self.th_price = 95 + self.price
-
-    def __str__(self):
-        return f'Thursday pizza: {self.th_name}\n' \
-               f'Ingredients: {self.th_basic}\n' \
-               f'Total price: {self.th_price}'
+class WhiteChicken(Pizza):
+    def __init__(self):
+        self.title = 'Wednesday: white chicken'
+        self.ingredients = ['sauce', 'cheese', 'onion', 'tomato', 'chicken', 'bacon']
+        self.price = 99
+        super().__init__(self.title, self.ingredients, self.price)
 
 
-class Friday(Pizza):
-    def __init__(self, *args):
-        super().__init__(*args)
-        self.f_name = 'chicken mushrooms'
-        self.f_basic = self.basic + ['onion', 'mushrooms', 'chicken', 'bacon'] + self.extra
-        self.f_price = 89 + self.price
-
-    def __str__(self):
-        return f'Friday pizza: {self.f_name}\n' \
-               f'Ingredients: {self.f_basic}\n' \
-               f'Total price: {self.f_price}'
+class Hawaiian(Pizza):
+    def __init__(self):
+        self.title = 'Thursday: hawaiian'
+        self.ingredients = ['sauce', 'cheese', 'onion', 'pineapple', 'chicken']
+        self.price = 95
+        super().__init__(self.title, self.ingredients, self.price)
 
 
-class Saturday(Pizza):
-    def __init__(self, *args):
-        super().__init__(*args)
-        self.sat_name = 'margarita'
-        self.sat_basic = self.basic + ['tomato'] + self.extra
-        self.sat_price = 69 + self.price
-
-    def __str__(self):
-        return f'Saturday pizza: {self.sat_name}\n' \
-               f'Ingredients: {self.sat_basic}\n' \
-               f'Total price: {self.sat_price}'
+class ChickenMushrooms(Pizza):
+    def __init__(self):
+        self.title = 'Friday: chicken mushrooms'
+        self.ingredients = ['sauce', 'cheese', 'onion', 'mushrooms', 'chicken', 'bacon']
+        self.price = 89
+        super().__init__(self.title, self.ingredients, self.price)
 
 
-class Sunday(Pizza):
-    def __init__(self, *args):
-        super().__init__(*args)
-        self.s_name = 'pepperoni'
-        self.s_basic = self.basic + ['salami', 'pepper'] + self.extra
-        self.s_price = 135 + self.price
-
-    def __str__(self):
-        return f'Sunday pizza: {self.s_name}\n' \
-               f'Ingredients: {self.s_basic}\n' \
-               f'Total price: {self.s_price}'
+class Margarita(Pizza):
+    def __init__(self):
+        self.title = 'Saturday: margarita'
+        self.ingredients = ['sauce', 'cheese', 'tomato']
+        self.price = 69
+        super().__init__(self.title, self.ingredients, self.price)
 
 
-pizza = Tuesday('paprika')
-print(pizza)
+class Pepperoni(Pizza):
+    def __init__(self):
+        self.title = 'Sunday: pepperoni'
+        self.ingredients = ['sauce', 'cheese', 'salami', 'pepper']
+        self.price = 135
+        super().__init__(self.title, self.ingredients, self.price)
+
+
+pizzas = {1: FourMeats, 2: FourCheeses, 3: WhiteChicken, 4: Hawaiian, 5: ChickenMushrooms, 6: Margarita, 7: Pepperoni}
+
+
+def return_pizza(day):
+    for item in pizzas:
+        if item == day:
+            return pizzas[item]
+
+
+now = datetime.now()
+week_day = datetime.isoweekday(now)
+
+pizza_of_the_day = return_pizza(week_day)
+pizza1 = pizza_of_the_day()
+pizza1.agg_ingredients('chicken')
+print(pizza1)
